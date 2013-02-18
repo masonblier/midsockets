1dfb64038680847191b28153d5853a31ac300753
Mason Blier <mason.blier@gmail.com>
2013-02-18 11:08:31 -0700

    start integrating yuidoc

---
+++ docs/api/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DeferredResponse",
+        "SockjsServerApp"
+    ],
+    "modules": [
+        "midsockets"
+    ],
+    "allModules": [
+        {
+            "displayName": "midsockets",
+            "name": "midsockets",
+            "description": "midsockets - real time streaming request promises"
+        }
+    ]
+} };
+});
