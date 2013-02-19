4e9b778258f8793f0541baf7d4c7f4f81f63bd5e
Mason Blier <mason.blier@gmail.com>
2013-02-19 06:50:56 -0700

    class yuidocs

--- api/api.js
+++ api/api.js
@@ -1,17 +1,33 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "App",
         "DeferredResponse",
-        "SockjsServer"
+        "Events",
+        "Inheritance",
+        "Middleware",
+        "MiddlewarePrototype",
+        "RequestClient",
+        "RequestGhost",
+        "RequestPromise",
+        "Router",
+        "SockjsClient",
+        "SockjsServer",
+        "Utils"
     ],
     "modules": [
+        "Promises",
         "midsockets"
     ],
     "allModules": [
         {
             "displayName": "midsockets",
-            "name": "midsockets",
-            "description": "midsockets - real time streaming request promises"
+            "name": "midsockets"
+        },
+        {
+            "displayName": "Promises",
+            "name": "Promises",
+            "description": "The class of the object passed as `res` to Router event handlers. When the\ndeferredResponse is resolved, the data is sent to the corresponding RequestPromise\non the client."
         }
     ]
 } };
